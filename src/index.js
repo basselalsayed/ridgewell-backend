c70b1b3e8ebf6b8564065f57b13280a980c20baf
basselalsayed <alsayed.bassel@gmail.com>
2020-07-08 09:07:04 +0100

    modular routing using express

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import express from 'express';
 import { v4 as uuidv4 } from 'uuid';
 import models from './models';
+import routes from './routes';
 
 const app = express();
 
@@ -18,63 +19,14 @@ app.use((req, res, next) => {
   };
   next();
 });
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/session', (req, res) =>
-  res.send(req.context.models.users[req.user.id]),
-);
-
-app.get('/users', (req, res) =>
-  res.send(Object.values(req.context.models.users)),
-);
-
-app.get('/users/:userId', (req, res) =>
-  res.send(req.context.models.users[req.params.userId]),
-);
-
-app.post('/users', (req, res) => res.send('POST HTTP method on user resource'));
-
-app.put('/users/:userId', (req, res) =>
-  res.send(`PUT HTTP method on user/${req.params.userId} resource`),
-);
-
-app.delete('/users/:userId', (req, res) =>
-  res.send(`DELETE HTTP method on user/${req.params.userId} resource`),
-);
-
-app.get('/messages', (req, res) =>
-  res.send(Object.values(req.context.models.messages)),
-);
-
-app.get('/messages/:messageId', (req, res) =>
-  res.send(req.context.models.messages[req.params.messageId]),
-);
-
-app.post('/messages', (req, res) => {
-  const id = uuidv4();
-  const message = {
-    id,
-    text: req.body.text,
-    userId: req.context.user.id,
-  };
-
-  req.context.models.messages[id] = message;
-
-  return res.send(message);
-});
-
-app.delete('/messages/:messageId', (req, res) => {
-  const {
-    [req.params.messageId]: message,
-    ...otherMessages
-  } = req.context.models.messages;
-
-  req.context.models.messages = otherMessages;
-
-  return res.send(message);
-});
+app.use('/session', routes.session);
+app.use('/users', routes.user);
+app.use('/messages', routes.message);
 
 app.listen(process.env.PORT, () =>
   console.log(`Example app listening on port ${process.env.PORT}!`),
