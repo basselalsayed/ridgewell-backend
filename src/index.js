e82cc15712022be3f1d1582d5aa9466fbdca6cf4
basselalsayed <alsayed.bassel@gmail.com>
2020-07-08 08:23:44 +0100

    added crud restful routing

--- src/index.js
+++ src/index.js
@@ -1,5 +1,59 @@
 import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
 
-console.log('Hello Node.js project.');
+const app = express();
 
-console.log(process.env.MY_SECRET);
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+let users = {
+  1: {
+    id: '1',
+    username: 'Robin Wieruch',
+  },
+  2: {
+    id: '2',
+    username: 'Dave Davids',
+  },
+};
+
+let messages = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'Bye World',
+    userId: '2',
+  },
+};
+
+app.get('/users', (req, res) => res.send(Object.values(users)));
+
+app.get('/users/:userId', (req, res) => res.send(users[req.params.userId]));
+
+app.post('/users', (req, res) => res.send('POST HTTP method on user resource'));
+
+app.put('/users/:userId', (req, res) =>
+  res.send(`PUT HTTP method on user/${req.params.userId} resource`),
+);
+
+app.delete('/users/:userId', (req, res) =>
+  res.send(`DELETE HTTP method on user/${req.params.userId} resource`),
+);
+
+app.get('/messages', (req, res) => res.send(Object.values(messages)));
+
+app.get('/messages/:messageId', (req, res) =>
+  res.send(messages[req.params.messageId]),
+);
+
+app.listen(process.env.PORT, () =>
+  console.log(`Example app listening on port ${process.env.PORT}!`),
+);
