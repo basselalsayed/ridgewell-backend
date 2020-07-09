726bc38e786aa27166ceba2603d077b6066e2725
basselalsayed <alsayed.bassel@gmail.com>
2020-07-09 19:23:57 +0100

    Validation to permit only one holiday in a given date range

--- src/controllers/holiday.js
+++ src/controllers/holiday.js
@@ -1,5 +1,6 @@
 import { handleError } from './helpers';
-
+import { Sequelize } from 'sequelize';
+const Op = Sequelize.Op;
 const getAll = async (req, res) =>
   res.send({ holidays: await req.context.models.Holiday.findAll() });
 
@@ -21,14 +22,34 @@ const updateHoliday = async (req, res) => {
     .catch(err => handleError(err));
 };
 
-const newHoliday = async (req, res) =>
-  res.send({
-    holiday: await req.context.models.Holiday.create({
-      from: req.body.from,
-      until: req.body.until,
-      userId: req.body.userId,
-    }),
-  });
+const newHoliday = async (req, res) => {
+  const where = {
+    [Op.or]: [
+      {
+        from: {
+          [Op.between]: [req.body.from, req.body.until],
+        },
+      },
+      {
+        until: {
+          [Op.between]: [req.body.from, req.body.until],
+        },
+      },
+    ],
+  };
+
+  await req.context.models.Holiday.findAll({ where }).then(async response =>
+    response.length
+      ? res.status(500).send({ message: 'Holiday already present' })
+      : res.status(200).send({
+          holiday: await req.context.models.Holiday.create({
+            from: req.body.from,
+            until: req.body.until,
+            userId: req.body.userId,
+          }),
+        }),
+  );
+};
 
 const deleteHoliday = async (req, res) =>
   await req.context.models.Holiday.destroy({
