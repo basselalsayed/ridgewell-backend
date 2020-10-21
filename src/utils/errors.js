fb2695a5512443d4bbdeea8a8c95d9783df515ca
basselalsayed <alsayed.bassel@gmail.com>
2020-10-21 22:31:47 +0100

    added NotUpdated to error class

--- src/utils/errors.js
+++ src/utils/errors.js
@@ -11,11 +11,16 @@ class GeneralError extends Error {
     if (this instanceof NotFound) {
       return 404;
     }
+    if (this instanceof NotUpdated) {
+      return 404;
+    }
+
     return 500;
   }
 }
 
 class BadRequest extends GeneralError {}
 class NotFound extends GeneralError {}
+class NotUpdated extends GeneralError {}
 
-export { GeneralError, BadRequest, NotFound };
+export { GeneralError, BadRequest, NotFound, NotUpdated };
