efdc4ca9ba6bc3f8e5c367112356271ca3278994
basselalsayed <alsayed.bassel@gmail.com>
2020-10-22 22:29:32 +0100

    created new seperated signin controller

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,4 +1,11 @@
+import { UserInteractor } from '../utils/interactors';
+import { compareSync } from 'bcryptjs';
+import { Unauthorized } from '../utils/errors';
+import { buildUserObjectResponse, send } from './helpers';
+
 import { signInService, signUpService } from '../services/auth';
+
+const userInteractor = new UserInteractor();
 
 const signUp = async (req, res) => {
   await signUpService(req, res);
@@ -8,4 +15,21 @@ const signIn = async (req, res) => {
   await signInService(req, res);
 };
 
-export { signIn, signUp };
+const signInNew = async (req, res, next) => {
+  const {
+    body: { username, email, password },
+  } = req;
+
+  try {
+    const user = await userInteractor.getUser({ username, email });
+
+    if (compareSync(password, user.password)) {
+      send(200, res, buildUserObjectResponse(user));
+      next();
+    } else throw new Unauthorized('Invalid Password');
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { signIn, signInNew, signUp };
