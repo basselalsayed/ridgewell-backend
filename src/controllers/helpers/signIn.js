90a058adaea02bc8af72c1d5c0c81b7dad468ee9
basselalsayed <alsayed.bassel@gmail.com>
2020-10-24 14:02:51 +0100

    reshuffling

--- src/controllers/helpers/signIn.js
+++ src/controllers/helpers/signIn.js
@@ -1,4 +1,3 @@
-import { sign } from 'jsonwebtoken';
 import { send } from '.';
 
 const noUser = res => send(404, res, { message: 'User not found.' });
@@ -7,11 +6,6 @@ const invalidPass = res =>
   send(401, res, {
     accessToken: null,
     message: 'Invalid Password!',
-  });
-
-const getAccessToken = id =>
-  sign({ id }, process.env.MY_SECRET, {
-    expiresIn: 86400, // 24 hours
   });
 
 const validPass = (res, user) => {
@@ -37,29 +31,4 @@ const validPass = (res, user) => {
     .then(obj => send(200, res, obj));
 };
 
-const buildUserObjectResponse = user => {
-  const { email, id, Roles, username } = user;
-
-  let accessToken = getAccessToken(id);
-
-  let roles = Roles.map(role => `ROLE_${role.name.toUpperCase()}`);
-
-  return {
-    message: 'Success!',
-    user: {
-      id,
-      username,
-      email,
-      roles,
-      accessToken,
-    },
-  };
-};
-
-export {
-  buildUserObjectResponse,
-  getAccessToken,
-  invalidPass,
-  noUser,
-  validPass,
-};
+export { invalidPass, noUser, validPass };
