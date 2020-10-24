ba61152a70770842c556e5ecaab4d940f756f6a4
basselalsayed <alsayed.bassel@gmail.com>
2020-10-24 13:45:58 +0100

    added newUser method

--- src/utils/interactors/UserInteractor.js
+++ src/utils/interactors/UserInteractor.js
@@ -1,4 +1,5 @@
 import { Interactor } from './Interactor';
+import { verifyRole } from '../../controllers/helpers';
 
 class UserInteractor extends Interactor {
   constructor() {
@@ -11,6 +12,20 @@ class UserInteractor extends Interactor {
     return await this.sequelize.transaction(async () =>
       this.User.findByLogin(login),
     );
+  }
+
+  async newUser({ username, email, password, roles }) {
+    return await this.sequelize.transaction(async () => {
+      const user = await this.User.create({
+        username,
+        email,
+        password,
+      });
+
+      await verifyRole(user, roles);
+
+      return await this.getUser({ username });
+    });
   }
 }
 export { UserInteractor };
