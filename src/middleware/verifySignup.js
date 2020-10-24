e83e4eb94dcfa80d3787510e483208a104de1177
basselalsayed <alsayed.bassel@gmail.com>
2020-10-24 16:43:21 +0100

    rebuilt checkDuplicateUsernameOrEmail middleware

--- src/middleware/verifySignup.js
+++ src/middleware/verifySignup.js
@@ -1,39 +1,21 @@
-import db from '../../database/models';
 import { NotFound } from '../utils/errors';
+import { UserInteractor } from '../utils/interactors';
 
-const { User } = db.sequelize.models;
 const ROLES = ['user', 'admin'];
 
-const checkDuplicateUsernameOrEmail = (req, res, next) => {
-  // Username
-  User.findOne({
-    where: {
-      username: req.body.username,
-    },
-  }).then(user => {
-    if (user) {
-      res.status(400).send({
-        message: 'Failed! Username is already in use!',
-      });
-      return;
-    }
+const checkDuplicateUsernameOrEmail = (
+  { body: { email, username } },
+  res,
+  next,
+) => {
+  const userInteractor = new UserInteractor();
 
-    // Email
-    User.findOne({
-      where: {
-        email: req.body.email,
-      },
-    }).then(user => {
-      if (user) {
-        res.status(400).send({
-          message: 'Failed! Email is already in use!',
-        });
-        return;
-      }
-
-      next();
-    });
-  });
+  Promise.all([
+    userInteractor.checkUsernameExists(username),
+    userInteractor.checkEmailExists(email),
+  ])
+    .then(() => next())
+    .catch(error => next(error));
 };
 
 const checkRolesExist = ({ body: { roles } }, res, next) => {
