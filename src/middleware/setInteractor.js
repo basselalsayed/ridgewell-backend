25f0b15afbbb77e4e0050051a70946387348724a
basselalsayed <alsayed.bassel@gmail.com>
2020-10-27 17:05:14 +0000

    improved setInteractor logic

--- src/middleware/setInteractor.js
+++ src/middleware/setInteractor.js
@@ -1,9 +1,12 @@
 import { HolidayInteractor, UserInteractor } from '../utils/interactors';
 
+const includes = (req, path) => req.path.includes(path);
+
 const setInteractor = async (req, res, next) => {
-  if (req.path === '/session' || req.path === '/users')
+  if (includes(req, 'session') || includes(req, 'users'))
     req.userInteractor = new UserInteractor();
-  if (req.path === '/holidays') req.holidayInteractor = new HolidayInteractor();
+  if (includes(req, 'holidays'))
+    req.holidayInteractor = new HolidayInteractor();
 
   next();
 };
