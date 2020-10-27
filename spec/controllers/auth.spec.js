b781418fce63a823fe7e63d0a0c37c93a1dbc5f0
basselalsayed <alsayed.bassel@gmail.com>
2020-10-27 11:54:58 +0000

    refactored auth controller tests

--- spec/controllers/auth.spec.js
+++ spec/controllers/auth.spec.js
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 import { stub, restore } from 'sinon';
 import { helpers } from 'faker';
 
-import { mockNext, mockReq, mockUser, res } from './mocks';
+import { mockNext, mockReq, res } from './mocks';
 
 import * as bcrypt from 'bcryptjs';
 import * as Helpers from '../../src/controllers/helpers';
@@ -21,16 +21,21 @@ describe(chalk.yellow('src/controllers/auth'), () => {
     },
     ...mockReq,
     userInteractor: {
+      getUser: stub().resolves(true),
       newUser: stub().resolves(true),
     },
   };
 
-  context('sign up', () => {
-    let buildUserObjectResponseStub;
-    let sendStub;
+  let buildUserObjectResponseStub;
+  let sendStub;
+
+  before(async () => {
+    buildUserObjectResponseStub = stub(Helpers, 'buildUserObjectResponse');
+    sendStub = stub(Helpers, 'send');
+  });
+
+  context('successful sign up', () => {
     before(async () => {
-      buildUserObjectResponseStub = stub(Helpers, 'buildUserObjectResponse');
-      sendStub = stub(Helpers, 'send');
       await signUp(req, res, mockNext);
     });
 
@@ -44,13 +49,48 @@ describe(chalk.yellow('src/controllers/auth'), () => {
         roles: ['user'],
       });
     });
+
+    it('called send', async () => {
+      expect(sendStub).to.have.been.called;
+    });
+
     it('called buildUserObjectResponseStub', async () => {
       expect(buildUserObjectResponseStub).to.have.been.called;
+    });
+
+    it('called next', async () => {
+      expect(mockNext).to.have.been.called;
+    });
+  });
+
+  context('successful signin', () => {
+    let bcryptStub;
+    before(async () => {
+      bcryptStub = stub(bcrypt.default, 'compareSync').returns(true);
+      await signIn(req, res, mockNext);
+    });
+
+    after(restore);
+
+    it('calls userInteractor.getUser', () => {
+      expect(req.userInteractor.getUser).to.have.been.calledWith({
+        email,
+        username,
+      });
+    });
+
+    it('calls bcryptStub', () => {
+      expect(bcryptStub).to.have.been.called;
     });
 
     it('called send', async () => {
       expect(sendStub).to.have.been.called;
     });
+
+    it('called buildUserObjectResponseStub', async () => {
+      expect(buildUserObjectResponseStub).to.have.been.called;
+    });
+
     it('called next', async () => {
       expect(mockNext).to.have.been.called;
     });
@@ -59,6 +99,7 @@ describe(chalk.yellow('src/controllers/auth'), () => {
   context('unsuccessful signin', () => {
     before(async () => {
       stub(bcrypt.default, 'compareSync').returns(false);
+      await signIn(req, res, mockNext);
     });
 
     after(restore);
