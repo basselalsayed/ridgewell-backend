3d2e8700fb4109e8f2679d3fef7eb18bab6a0107
basselalsayed <alsayed.bassel@gmail.com>
2020-10-27 15:12:07 +0000

    updated setInteractor middleware

--- src/middleware/setInteractor.js
+++ src/middleware/setInteractor.js
@@ -1,8 +1,9 @@
-import { UserInteractor } from '../utils/interactors';
+import { HolidayInteractor, UserInteractor } from '../utils/interactors';
 
 const setInteractor = async (req, res, next) => {
   if (req.path === '/session' || req.path === '/users')
     req.userInteractor = new UserInteractor();
+  if (req.path === '/holidays') req.holidayInteractor = new HolidayInteractor();
 
   next();
 };
