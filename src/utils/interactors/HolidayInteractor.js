1da69d51e17b9a5c8f2194935009dde3fd4d3f18
basselalsayed <alsayed.bassel@gmail.com>
2020-10-27 15:00:47 +0000

    getAllHolidays function

--- src/utils/interactors/HolidayInteractor.js
+++ src/utils/interactors/HolidayInteractor.js
@@ -1,9 +1,35 @@
+import Holiday from '../../../database/models/Holiday';
 import { Interactor } from './Interactor';
 
 class HolidayInteractor extends Interactor {
   constructor() {
     super();
   }
+
+  async getAllHolidays() {
+    return await this.sequelize.transaction(
+      async () =>
+        await this.Holiday.findAll({
+          include: [
+            {
+              model: this.HolidayRequest,
+              attributes: ['id', 'type', 'from', 'until', 'resolved'],
+            },
+            {
+              model: this.User,
+              attributes: ['username', 'email'],
+            },
+          ],
+        }),
+    );
+  }
 }
 
 export { HolidayInteractor };
+
+let holidayInteractor = new HolidayInteractor();
+
+(async function () {
+  const holiday = await holidayInteractor.getAllHolidays();
+  console.log(holiday);
+})();
