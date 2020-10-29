d408db3b99a5a5da61c7e0685a391bdd49358e4e
basselalsayed <alsayed.bassel@gmail.com>
2020-10-29 16:32:08 +0000

    extracted deleteExisting Requests function

--- src/utils/interactors/RequestInteractor.js
+++ src/utils/interactors/RequestInteractor.js
@@ -5,28 +5,33 @@ class RequestInteractor extends Interactor {
     super();
   }
 
+  async deleteExisting(transaction, holidayId) {
+    await this.HolidayRequest.destroy({
+      transaction,
+      where: { holidayId },
+    });
+  }
+
   async newUpdate(from, holidayId, until, owner) {
-    return await this.sequelize.transaction(
-      async transaction =>
-        await this.HolidayRequest.create(
-          {
-            holidayId,
-            from,
-            type: 'update',
-            until,
-            owner,
-          },
-          { transaction },
-        ),
-    );
+    return await this.sequelize.transaction(async transaction => {
+      await this.deleteExisting(transaction, holidayId);
+
+      return await this.HolidayRequest.create(
+        {
+          holidayId,
+          from,
+          type: 'update',
+          owner,
+          until,
+        },
+        { transaction },
+      );
+    });
   }
 
   async newDelete(holidayId, owner) {
     return await this.sequelize.transaction(async transaction => {
-      await this.HolidayRequest.destroy({
-        transaction,
-        where: { holidayId },
-      });
+      await this.deleteExisting(transaction, holidayId);
 
       return await this.HolidayRequest.create(
         {
