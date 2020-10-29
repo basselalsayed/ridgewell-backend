f028689c21b3140b09bbce0b3fb5f66f048394c9
basselalsayed <alsayed.bassel@gmail.com>
2020-10-29 13:52:15 +0000

    RequestInteractor.newUpdate request function

--- src/utils/interactors/RequestInteractor.js
+++ src/utils/interactors/RequestInteractor.js
@@ -4,6 +4,21 @@ class RequestInteractor extends Interactor {
   constructor() {
     super();
   }
+
+  async newUpdate(from, holidayId, until, owner) {
+    return await this.sequelize.transaction(async transaction => {
+      return await this.HolidayRequest.create(
+        {
+          holidayId,
+          from,
+          type: 'update',
+          until,
+          owner,
+        },
+        { transaction },
+      );
+    });
+  }
 }
 
 export { RequestInteractor };
