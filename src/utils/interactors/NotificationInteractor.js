e5564557977d55a94d32d8d388a5eee6800ab291
basselalsayed <alsayed.bassel@gmail.com>
2020-11-09 16:23:57 +0000

    NotificationInteractor

---
+++ src/utils/interactors/NotificationInteractor.js
@@ -0,0 +1,38 @@
+import { Conflict, NotUpdated } from '../errors';
+import { Interactor } from './Interactor';
+
+class NotificationInteractor extends Interactor {
+  constructor() {
+    super();
+  }
+
+  async getAll(userId) {
+    return await this.sequelize.transaction(
+      async transaction =>
+        await this.Notification.findAll({
+          where: { userId },
+          attributes: {
+            exclude: ['userId'],
+          },
+          transaction,
+        }),
+    );
+  }
+
+  async update(read, id) {
+    return await this.sequelize.transaction(async transaction => {
+      const response = await this.Holiday.update(
+        { read },
+        {
+          transaction,
+          where: { id },
+        },
+      );
+
+      if (response && response[0] > 0) return response;
+      else throw new NotUpdated('Nothing was updated');
+    });
+  }
+}
+
+export { NotificationInteractor };
