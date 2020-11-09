1f502b56344bbb01b1f6342dadc8e468d8776f66
basselalsayed <alsayed.bassel@gmail.com>
2020-11-09 16:32:22 +0000

    notifications controller

---
+++ src/controllers/notification.js
@@ -0,0 +1,32 @@
+import { send } from './helpers';
+
+const getAllNotifications = async (
+  { notificationInteractor, query: { userId } },
+  res,
+  next,
+) => {
+  try {
+    const notifications = await notificationInteractor.getAll(userId);
+
+    if (notifications) send(200, res, notifications);
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateNotification = async (
+  { body, notificationInteractor, params: { notificationId } },
+  res,
+  next,
+) => {
+  try {
+    await notificationInteractor.update(body, notificationId);
+
+    send(200, res, { message: 'Success' });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { getAllNotifications, updateNotification };
