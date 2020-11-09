b1ea67988e689978c64eec03cc0ce1d0a05ba703
basselalsayed <alsayed.bassel@gmail.com>
2020-11-09 15:21:43 +0000

    removed admin middleware to get requests

--- src/routes/holidayRequest.js
+++ src/routes/holidayRequest.js
@@ -11,7 +11,7 @@ import {
 
 const router = Router();
 
-router.get('/', [verifyToken, isAdmin], getAllRequests);
+router.get('/', [verifyToken], getAllRequests);
 
 router.post('/', [verifyToken, isOwner], newRequest);
 
